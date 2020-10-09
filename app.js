0ad052190ef482f06dbc4145d7562584b8de6308
Tyler <tbrinkman3@gmail.com>
2020-10-08 22:33:03 -0500

    Added the hover color change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 
+//Create grid
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -11,3 +12,12 @@ function makeRows(rows, cols) {
 }
 
 makeRows(16,16);
+
+//Fill in squares
+const squares = document.querySelectorAll('.grid-item');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'blue';
+    })
+})
